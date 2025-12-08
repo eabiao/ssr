aa56f216ab4d88e0db15141ffc6e69a538ddf4f4
eabiao <eabiao@163.com>
2025-12-08 21:02:01 +0800

    Update test01.js

--- test01.js
+++ test01.js
@@ -1,1 +1,18 @@
-console.log("aaa");
+const listUrl = process.env.URL_01;
+const saveUrl = process.env.URL_02;
+
+const cmsHeaders = {
+  "token": process.env.TOKEN
+};
+
+const ssrHeaders = {
+  'Accept':'*/*',
+  'User-Agent':'clash-verge/v1.7.5'
+};
+
+let ssrUrl = "";
+
+const resp01 = await fetch(listUrl, {headers});
+
+console.log("headers", await resp01.headers);
+console.log("headers", await resp01.text());
